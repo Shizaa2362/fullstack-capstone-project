d5ee11fe11d00ffc1d4bf3d70c77b13425f46375
Shizaa2362 <shizafaheem23@gmail.com>
2026-09-20 19:00:58 +0500

    Add MongoDB connection setup in db.js

--- db.js
+++ db.js
@@ -1,1 +1,15 @@
+const { MongoClient } = require("mongodb");
 
+const uri = process.env.MONGODB_URI || "mongodb://localhost:27017";
+const client = new MongoClient(uri);
+
+async function connectToDatabase() {
+await client.connect();
+console.log("Connected to MongoDB");
+return client.db("giftlink");
+}
+
+module.exports = {
+client,
+connectToDatabase
+};
