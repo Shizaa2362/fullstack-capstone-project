48fbd587d3cc7d06f4b397f7fc324e18c95daaca
Shizaa2362 <shizafaheem23@gmail.com>
2026-09-20 19:03:31 +0500

    Set up Express server with natural language processing

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const natural = require("natural");
+const express = require("express");
 
+const app = express();
+
+app.use(express.json());
+
+const tokenizer = new natural.WordTokenizer();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "GiftLink application is running",
+    naturalLoaded: !!natural,
+    tokenizerExample: tokenizer.tokenize("GiftLink application")
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`GiftLink server running on port ${PORT}`);
+});
