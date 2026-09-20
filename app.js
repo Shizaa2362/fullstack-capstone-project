47f2d8dc62a1cb349913496bc139e6da480d9bd6
Shizaa2362 <shizafaheem23@gmail.com>
2026-09-20 19:02:48 +0500

    Set up Express server with routes and middleware

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 
+const express = require("express");
+const cors = require("cors");
+
+const giftRoutes = require("./giftRoutes");
+const searchRoutes = require("./searchRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(giftRoutes);
+app.use("/api/search", searchRoutes);
+
+app.get("/", (req, res) => {
+  res.send("GiftLink API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
